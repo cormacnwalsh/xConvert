aaf6db8ad312781a19429581e9d32ee091a56fac
cormacnwalsh <cormacnwalsh@gmail.com>
2017-12-20 13:57:40 +0000

    Merge origin/master into Cormac
    Conflicts:
    	src/xconvert/Weight.java

--- src/xconvert/XConvert.java
+++ src/xconvert/XConvert.java
@@ -1,16 +1,18 @@
-
-
 package xconvert;
 
 import javax.swing.JOptionPane;
 
-public class XConvert 
-{
+public class XConvert {
 
-    public static void main(String[] args) 
-    {
-        Object [] options = {"Shite way","Fancy way","Neither way"};
-        int choice = JOptionPane.showOptionDialog(null,
+    public static void main(String[] args) {
+        boolean select = true;
+        
+        while(select){
+            
+
+            for(int i = 0; i < 1;){
+                Object[] options = {"Shite way", "Fancy way", "Neither way"};
+                int choice = JOptionPane.showOptionDialog(null,
                 "Select shite way or fancy way.",
                 "How?",
                 JOptionPane.YES_NO_OPTION,
@@ -18,30 +20,28 @@ public class XConvert
                 null,
                 options,
                 options[2]);
-        
-        boolean open = true;
-        
-        while(open){
-            if(choice == 0){
-                Convert c = new Convert();
-                c.start();
                 
-            }else if(choice == 1){
-                convertForm f = new convertForm();
-                f.setVisible(true);
-                
-            }else if(choice == 2){
-                open = false;
-                
+                if (choice == 0) {
+                    Convert c = new Convert();
+                    c.start();
+
+                } else if (choice == 1) {
+                    convertForm f = new convertForm();
+                    f.setVisible(true);
+
+                } else if (choice == 2) {
+                    i++;
+                    select = false;
+                }
             }
-        }
         
+        }
+
         JOptionPane.showMessageDialog(null, "Shutting down");
 
         JOptionPane.showMessageDialog(null, "Douchecanoe!");
-        
-    } //End Main
-    
-}//End Class
 
+    } //End Main
+
+}//End Class
 
